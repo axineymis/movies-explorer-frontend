0c52fb863c43ec4b5cbbbce524328c70e22cdc20
axineymis <otm-natasha.biryukova@yandex.ru>
2022-07-22 15:51:34 +0300

    fix routes

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,27 +1,39 @@
 import React, { useState } from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import Main from '../Main/Main';
-// import Movies from '../Movies/Movies';
-// import SavedMovies from '../SavedMovies/SavedMovies';
-// import Profile from '../Profile/Profile';
-// import Login from '../Auth/Login';
-// import Register from '../Auth/Register';
-// import NotFound from '../NotFound/NotFound';
+import Movies from '../Movies/Movies';
+import SavedMovies from '../SavedMovies/SavedMovies';
+import Profile from '../Profile/Profile';
+import Login from '../Auth/Login';
+import Register from '../Auth/Register';
+import NotFound from '../NotFound/NotFound';
 import './App.css';
 
 function App() {
   const [loggedIn] = useState(true);
   return (
     <div className='page'>
-      <Routes>
-       <Route exact path='/'
-       element={
-        <>
+      <Switch>
+        <Route exact path='/'>
           <Main loggedIn={loggedIn} />
-          </>
-         }
-         />
-      </Routes>
+        </Route>
+        <Route path='/movies'>
+          <Movies loggedIn={loggedIn} />
+        </Route>
+        <Route path='/saved-movies'>
+          <SavedMovies loggedIn={loggedIn} />
+        </Route>
+        <Route path='/profile'>
+          <Profile />
+        </Route>
+        <Route path='/signin' component={Login}></Route>
+        <Route
+          path='/signup'
+          component={Register}
+          buttonText='Зарегистрироваться'
+        ></Route>
+        <Route path='*' component={NotFound}></Route>
+      </Switch>
     </div>
   );
 }
